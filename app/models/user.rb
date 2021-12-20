fe6463c825759b3a13f041d1d5e97110b03818c3
Regi Fronda <regisfronda@gmail.com>
2021-12-19 22:29:05 -0800

    Add devise model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { maximum: 20 }
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 6..20 }
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
